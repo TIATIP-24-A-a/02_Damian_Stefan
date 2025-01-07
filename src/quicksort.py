e6f6e5b86586089b8824f357a9247d8a2c6f5a9c
Stefan <bachmann.stefan@proton.me>
2025-01-07 20:30:27 +0100

    refactor: quicksort funktion überarbeitet.

--- src/quicksort.py
+++ src/quicksort.py
@@ -3,17 +3,12 @@ import numpy as np
 
 def quicksort(array: list) -> list:
     pivot: int
-    left: list
-    right: list
-    middle: list
 
     if len(array) <= 1:
         return array
 
     pivot = array[(len(array) // 2 )]
 
-    left   = [x for x in array if x <  pivot]
-    middle = [x for x in array if x == pivot]
-    right  = [x for x in array if x >  pivot]
-    print(left + middle + right)
-    return quicksort(left) + quicksort(middle) + quicksort(right)
+    return (quicksort([x for x in array if x <  pivot]) +
+            quicksort([x for x in array if x == pivot]) +
+            quicksort([x for x in array if x >  pivot]))
