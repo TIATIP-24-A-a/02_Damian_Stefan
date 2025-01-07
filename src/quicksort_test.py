e896410753d73fd07e967b0ae064c7a070a758b5
Stefan <bachmann.stefan@proton.me>
2025-01-07 20:22:54 +0100

    test: bugfix in test

--- src/quicksort_test.py
+++ src/quicksort_test.py
@@ -2,11 +2,9 @@ import unittest
 from quicksort import quicksort
 
 class TestSort(unittest.TestCase):
-    testInput:  list
-    testOutput: list
 
     # Test
     def testUnsorted(self):
-        test_input = [8, 3, 1, 7, 0, 10, 2]  # Arrange
-        test_output = quicksort(test_input)  # Act
-        self.assertEqual(test_output, test_input)  # Assert
+        test_input: list = [8, 3, 1, 7, 0, 10, 2]  # Arrange
+        test_output: list = quicksort(test_input)  # Act
+        self.assertEqual(test_output, [0, 1, 2, 3, 7, 8, 10])  # Assert
