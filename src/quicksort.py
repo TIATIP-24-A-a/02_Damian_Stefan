6011b02360dc55fef06aecac711e16eba00150de
Stefan <bachmann.stefan@proton.me>
2025-01-07 19:37:49 +0100

    pivot element implementiert

--- src/quicksort.py
+++ src/quicksort.py
@@ -1,4 +1,10 @@
 import numpy as np
+from fontTools.misc.cython import returns
 
-def quicksort():
-    print("foo")
+
+def quicksort(array: list) -> list:
+    if len(array) <= 1:
+        return array
+    pivot = array[(len(array) // 2 )]
+
+    return array
