d582fad7028be74bd945b16dd08913fd94ad1426
Stefan <bachmann.stefan@proton.me>
2025-01-07 19:52:08 +0100

    test: test refactoring

--- src/quicksort_test.py
+++ src/quicksort_test.py
@@ -2,11 +2,11 @@ import unittest
 from quicksort import quicksort
 
 class TestSort(unittest.TestCase):
-    input: list
-    output: list
+    testInput:  list
+    testOutput: list
 
     # Test
     def testUnsorted(self):
-        testInput = [8, 3, 1, 7, 0, 10, 2]  # Arrange
-        testOutput= quicksort(testInput)  # Act
-        self.assertEqual(testOutput, testInput)  # Assert
+        test_input = [8, 3, 1, 7, 0, 10, 2]  # Arrange
+        test_output = quicksort(test_input)  # Act
+        self.assertEqual(test_output, test_input)  # Assert
