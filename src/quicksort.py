61dfc33a001148a9e465aae8d26c6ae9fc470f87
Stefan <bachmann.stefan@proton.me>
2025-01-07 20:06:01 +0100

    feat: erste implementierung quicksort

--- src/quicksort.py
+++ src/quicksort.py
@@ -1,10 +1,19 @@
 import numpy as np
-from fontTools.misc.cython import returns
 
 
 def quicksort(array: list) -> list:
+    pivot: int
+    left: list
+    right: list
+    middle: list
+
     if len(array) <= 1:
         return array
+
     pivot = array[(len(array) // 2 )]
 
-    return array
+    left   = [x for x in array if x <  pivot]
+    middle = [x for x in array if x == pivot]
+    right  = [x for x in array if x >  pivot]
+    print(left + middle + right)
+    return quicksort(left) + quicksort(middle) + quicksort(right)
