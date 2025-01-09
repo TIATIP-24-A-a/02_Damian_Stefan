6bcbee3288ee3fc326aba873f805a8dcd8033ce6
Stefan <bachmann.stefan@proton.me>
2025-01-09 13:55:04 +0100

    refactor: Komentare gelöst

--- src/quicksort_test.py
+++ src/quicksort_test.py
@@ -5,6 +5,6 @@ class TestSort(unittest.TestCase):
 
     # Test
     def testUnsorted(self):
-        test_input: list = [8, 3, 1, 7, 0, 10, 2]  # Arrange
-        test_output: list = quicksort(test_input)  # Act
-        self.assertEqual(test_output, [0, 1, 2, 3, 7, 8, 10])  # Assert
+        test_input: list = [8, 3, 1, 7, 0, 10, 2]
+        test_output: list = quicksort(test_input)
+        self.assertEqual(test_output, [0, 1, 2, 3, 7, 8, 10])
