2bb983fba3e36bc7752475f684dd02504cb26ce0
stefan <stefan.bachmann@edu.teko.ch>
2025-01-09 14:27:28 +0100

    testing: tests Lehre liste und ein element getestet

--- src/quicksort_test.py
+++ src/quicksort_test.py
@@ -3,7 +3,16 @@ from quicksort import quicksort
 
 class TestSort(unittest.TestCase):
 
-    # Test
+    def testEmpty(self):
+        test_input: list = []
+        test_output: list = quicksort(test_input)
+        self.assertEqual(test_output, [])
+
+    def testOneElement(self):
+        test_input: list = [42]
+        test_output: list = quicksort(test_input)
+        self.assertEqual(test_output, [42])
+
     def testUnsorted(self):
         test_input: list = [8, 3, 1, 7, 0, 10, 2]
         test_output: list = quicksort(test_input)
