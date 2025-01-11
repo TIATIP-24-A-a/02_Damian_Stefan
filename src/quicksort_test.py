3cbaf2b76772fab86fbe3f569fe5c28306e386a1
Stefan <bachmann.stefan@proton.me>
2025-01-11 15:25:14 +0100

    test: tests angepast für neues Errorhandling

--- src/quicksort_test.py
+++ src/quicksort_test.py
@@ -4,57 +4,73 @@ from quicksort import quicksort
 class TestSort(unittest.TestCase):
 
     def testEmpty(self):
-        test_input: list = []
-        test_output: list = quicksort(test_input)
+        test_input = []
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [])
+        self.assertEqual(error, None)
 
     def testOneElement(self):
-        test_input: list = [42]
-        test_output: list = quicksort(test_input)
+        test_input = [42]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [42])
+        self.assertEqual(error, None)
 
     def testRandom(self):
-        test_input: list = [8, 3, 1, 7, 0, 10, 2]
-        test_output: list = quicksort(test_input)
+        test_input = [8, 3, 1, 7, 0, 10, 2]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [0, 1, 2, 3, 7, 8, 10])
+        self.assertEqual(error, None)
 
     def testSorted(self):
-        test_input: list = [1, 2, 3, 4, 5]
-        test_output: list = quicksort(test_input)
+        test_input = [1, 2, 3, 4, 5]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [1, 2, 3, 4, 5])
+        self.assertEqual(error, None)
 
     def testReverseSorted(self):
-        test_input: list = [5, 4, 3, 2, 1]
-        test_output: list = quicksort(test_input)
+        test_input = [5, 4, 3, 2, 1]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [1, 2, 3, 4, 5])
+        self.assertEqual(error, None)
 
     def testDubbledElements(self):
-        test_input: list = [3, 1, 2, 3, 4, 3]
-        test_output: list = quicksort(test_input)
+        test_input = [3, 1, 2, 3, 4, 3]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [1, 2, 3, 3, 3, 4])
+        self.assertEqual(error, None)
 
     def testNegativeElements(self):
-        test_input: list = [0, -1, 3, -5, 2]
-        test_output: list = quicksort(test_input)
+        test_input = [0, -1, 3, -5, 2]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [-5, -1, 0, 2, 3])
+        self.assertEqual(error, None)
 
     def testAllTheSame(self):
-        test_input: list = [7, 7, 7, 7]
-        test_output: list = quicksort(test_input)
+        test_input = [7, 7, 7, 7]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [7, 7, 7, 7])
+        self.assertEqual(error, None)
 
     def testManyDoubled(self):
-        test_input: list = [4, 5, 4, 4, 6, 4, 5, 5]
-        test_output: list = quicksort(test_input)
+        test_input = [4, 5, 4, 4, 6, 4, 5, 5]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [4, 4, 4, 4, 5, 5, 5, 6])
+        self.assertEqual(error, None)
 
     def testFloats(self):
-        test_input: list = [2.1, 3.5, 1.2, 5.7, 0.5]
-        test_output: list = quicksort(test_input)
+        test_input = [2.1, 3.5, 1.2, 5.7, 0.5]
+        test_output, error = quicksort(test_input)
         self.assertEqual(test_output, [0.5, 1.2, 2.1, 3.5, 5.7])
+        self.assertEqual(error, None)
+    def testMixedTypes(self):
+        test_input = ['D', '$', '&', 1, 7]
+        test_output, error = quicksort(test_input)
+        test_output, error = quicksort(test_input)
+        self.assertEqual(test_output, None)
+        self.assertEqual(error, Exception("Als Eingabe dürfen nur Zahlen verwendet werden!"))
 
-    def test_numbers_smaller_than_1_or_bigger_than_100(self):
-        with self.assertRaises(Exception) as context:
-            test_input: list = ['D', '$', '&', 1, 7]
-            quicksort(test_input)
-        self.assertEqual(str(context.exception), "Als Eingabe dürfen nur Zahlen verwendet werden!")
+    def testListOfStrings(self):
+        test_input = ['D', '$', '&', 'f', '5']
+        test_output, error = quicksort(test_input)
+        self.assertEqual(test_output, None)
+        self.assertEqual(error, Exception("Als Eingabe dürfen nur Zahlen verwendet werden!"))
