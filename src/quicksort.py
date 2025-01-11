6b7a7850a27676d34d512ec7bf65565045876e3e
Stefan <bachmann.stefan@proton.me>
2025-01-11 17:05:44 +0100

    refactor: funktionsaufruffe aus return call entfernt und type hints entfernt

--- src/quicksort.py
+++ src/quicksort.py
@@ -10,15 +10,17 @@ from requests.cookies import extract_cookies_to_jar
 # insertion sort on sorted parts
 
 
-def quicksort(array: list) -> tuple[list | None, Exception | None]:
+def quicksort(array: list):
     pivot: int
     left: list = []
     middle: list = []
     right: list = []
+
     if len(array) <= 1:
         return array, None
 
     pivot = array[(len(array) // 2 )]
+
     for x in array:
         if isinstance(x, int | float):
             if x < pivot:
@@ -30,4 +32,7 @@ def quicksort(array: list) -> tuple[list | None, Exception | None]:
         else:
             return None, Exception("Als Eingabe dürfen nur Zahlen verwendet werden!")
 
-        return (quicksort(left) +  middle + quicksort(right)), None
+        left = quicksort(left)
+        right = quicksort(right)
+
+        return left +  middle + right
